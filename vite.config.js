ab3a2c30418830b5b4f2a6661a74701d4119c67f
alanGarcia <220258@utags.edu.mx>
2025-08-11 12:12:13 -0600

    Fix build configuration for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import fs from "fs";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -12,13 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  /*   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "./certs/react-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "./certs/react-cert.pem")),
-    },
-    port: 3000,
-    host: true, // Habilita el acceso desde la red
-  }, */
-  assetsInclude: ["**/*.md"], // Incluir archivos Markdown como assets
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "esbuild",
+  },
+  assetsInclude: ["**/*.md"],
 });
