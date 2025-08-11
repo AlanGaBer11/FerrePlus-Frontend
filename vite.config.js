836234f395ecf697db5f483d54d7217207ea5a8a
alanGarcia <220258@utags.edu.mx>
2025-08-11 13:01:14 -0600

    fix: update vite config for tailwindcss and build optimization

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "tailwindcss";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
