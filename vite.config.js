a15f349962ab473de556a3c712597c3be14f3ec5
alanGarcia <220258@utags.edu.mx>
2025-08-11 13:53:56 -0600

    Revert "config"
    This reverts commit a1d5e6b058a45c0840aef6f9f6b3722028d8f307.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// filepath: d:\GitHub\FerrePlus\Frontend\interface\vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -13,21 +14,6 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: false,
     minify: "esbuild",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          utils: [
-            "@radix-ui/react-avatar",
-            "@radix-ui/react-dialog",
-            "@radix-ui/react-separator",
-          ],
-        },
-      },
-    },
   },
   assetsInclude: ["**/*.md"],
-  optimizeDeps: {
-    include: ["react", "react-dom"],
-  },
 });
