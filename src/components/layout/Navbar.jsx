7df508b6bd7ba70b3e44aba649368371e5ce622d
alanGarcia <220258@utags.edu.mx>
2025-08-11 15:04:12 -0600

    Refactor AuthService Import

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router";
 import "@/styles/Navbar.css";
 import logo from "@/assets/logo.png";
-import AuthService from "@/services/auth/authService";
+import AuthService from "@/services/auth/AuthService.jsx";
 import { useAuth } from "@/context/AuthContext";
 
 const Navbar = () => {
