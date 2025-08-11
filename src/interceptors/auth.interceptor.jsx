1939e829fa155cab409b51eb04fb3760a3635204
alanGarcia <220258@utags.edu.mx>
2025-08-11 16:56:55 -0600

    PROD

--- src/interceptors/auth.interceptor.jsx
+++ src/interceptors/auth.interceptor.jsx
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 import AuthService from "@/services/auth/AuthService";
 import ToastService from "@/services/toast/ToastService";
 
-const API_URL = import.meta.env.VITE_BASE_URL_LOCAL;
+const API_URL = import.meta.env.VITE_BASE_URL_PROD;
 
 // RUTAS QUE REQUIERES AUTENTICACIÓN
 const AUTH_ROUTES = [
